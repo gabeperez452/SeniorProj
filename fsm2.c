9e4472d768a926886b2aa389f28c4c9647587597
empty-window <70784520+empty-window@users.noreply.github.com>
2024-10-08 10:11:58 -0600

    Update fsm2.c

--- fsm2.c
+++ fsm2.c
@@ -1,6 +1,12 @@
 #include "stm32l552xx.h"
 #include <stdbool.h>
 
+// Some helper macros
+#define bitset(word,   idx)  ((word) |=  (1<<(idx))) //Sets the bit number <idx> -- All other bits are not affected.
+#define bitclear(word, idx)  ((word) &= ~(1<<(idx))) //Clears the bit number <idx> -- All other bits are not affected.
+#define bitflip(word,  idx)  ((word) ^=  (1<<(idx))) //Flips the bit number <idx> -- All other bits are not affected.
+#define bitcheck(word, idx)  ((word>>idx) & 1      ) //Checks the bit number <idx> -- 0 means clear; !0 means set.
+
 // Function prototypes for delay and states
 void delay_ms(uint32_t ms);
 void state_idle();
