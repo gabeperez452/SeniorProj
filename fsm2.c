fa59edf8863addc61031e3c5540f2816ce826c93
empty-window <70784520+empty-window@users.noreply.github.com>
2024-10-09 11:49:45 -0600

    Update fsm2.c

--- fsm2.c
+++ fsm2.c
@@ -1,25 +1,10 @@
 #include "stm32l552xx.h"
 #include <stdbool.h>
 
-// Some helper macros
-#define bitset(word,   idx)  ((word) |=  (1<<(idx))) //Sets the bit number <idx> -- All other bits are not affected.
-#define bitclear(word, idx)  ((word) &= ~(1<<(idx))) //Clears the bit number <idx> -- All other bits are not affected.
-#define bitflip(word,  idx)  ((word) ^=  (1<<(idx))) //Flips the bit number <idx> -- All other bits are not affected.
-#define bitcheck(word, idx)  ((word>>idx) & 1      ) //Checks the bit number <idx> -- 0 means clear; !0 means set.
-
-//
-void setClks()
-{
-    RCC->APB1ENR1 |=1<<28;   // Enable the power interface clock by setting the PWREN bits
-	RCC->APB1ENR2 |=0x1;     // Enable LPUART1EN clock
-	RCC->CCIPR1   |=0x800;   // 01 for HSI16 to be used for LPUART1
-	RCC->CCIPR1   &= ~(0x400);
-	RCC->CFGR     |=0x1;     // Use HSI16 as SYSCLK
-	RCC->CR       |=0x161;   // MSI clock enable; MSI=4 MHz; HSI16 clock enable
-}
-
 // Function prototypes for delay and states
 void delay_ms(uint32_t ms);
+//
+
 void state_idle();
 void state_checkHeadPosition();
 void state_checkBedPosition();
@@ -48,9 +33,6 @@ bool detect_error() {
 }
 
 int main() {
-
-    setClks();
-    
     // Enable clock for GPIOC (PC13 for the button) and GPIOB (PB7 for the LED)
     RCC->AHB2ENR |= (1 << 2);  // Enable clock for GPIOC (bit 2 for PC13)
     RCC->AHB2ENR |= (1 << 1);  // Enable clock for GPIOB (bit 1 for PB7)
@@ -68,6 +50,43 @@ int main() {
 
     // Disable the WKUP2 functionality to ensure PC13 works as a regular GPIO
     PWR->CR3 &= ~PWR_CR3_EWUP2;  // Clear the EWUP2 bit to disable WKUP2
+
+    // GPIOE clks
+	RCC->AHB2ENR |= 1<<4;
+
+	// PE 15, 14, 12, 10 as output
+	GPIOE->MODER &= ~(0b111100110011 << 20);
+	GPIOE->MODER |=  (0b010100010001 << 20);
+	// turn all off
+	GPIOE->ODR &=   ~(0b000000 << 10);
+
+
+	// setup PWM
+	// GPIOB clk
+	RCC->AHB2ENR |= 1 << 1;
+
+	// set PB11 as TIM2 CH2 af mode
+	GPIOB->MODER &= ~(0b11 << 22);
+	GPIOB->MODER |=  (0b10 << 22);
+	GPIOB->AFR[1] &= ~(0b1111 << 12);
+	GPIOB->AFR[1] |=  (0b0001 << 12);
+
+	// setup TIM2 CH4 for us pwm
+	RCC->APB1ENR1 |= (1 << 0); 		// clk
+
+	// cycle 	20ms 	(20000us)
+	// 0deg		.5ms	(500us)
+	// 90deg	1.5ms	(1500us)
+	// 180deg	2.5ms	(2500us)
+	// range	2ms		(2000us)
+
+	TIM2->PSC = 16000 - 1;		// prescale for 1 cycle = 1ms
+	TIM2->ARR = 1000 - 1;		// reset every 1000ms
+	TIM2->CCMR2 = 0x6000;		// set switch on matched value
+	TIM2->CCR4 = 500 - 1 ;		// switch on half for now
+	TIM2->CCER |= (0b1 << 12);	// enable CH4 Compare Mode
+	TIM2->CNT = 0;
+	TIM2->CR1 = 1;
 
     // Run FSM continuously
     while (1) {
@@ -87,6 +106,20 @@ void delay_ms(uint32_t ms) {
         __NOP();  // No operation, just burn time (adjust factor for your clock speed)
     }
 }
+//
+
+// Clock config and enable
+void setClks()
+{
+	RCC->APB1ENR1 |=1<<28;   // Enable the power interface clock by setting the PWREN bits
+	RCC->APB1ENR2 |=0x1;     // Enable LPUART1EN clock
+	RCC->CCIPR1   |=0x800;   // 01 for HSI16 to be used for LPUART1
+	RCC->CCIPR1   &= ~(0x400);
+	RCC->CFGR     |=0x1;     // Use HSI16 as SYSCLK
+	RCC->CR       |=0x161;   // MSI clock enable; MSI=4 MHz; HSI16 clock enable
+}
+//
+
 
 // State functions
 void state_idle() {
@@ -110,11 +143,33 @@ void state_idle() {
     }
 }
 
+
+
 void state_linearActuatorGoUp() {
     printf("State: Linear Actuator Go Up\n");
     // Simulate actuator movement (placeholder code)
-    delay_ms(2000);  // Simulate actuator movement time
+   // delay_ms(2000);  // Simulate actuator movement time
     GPIOB->ODR &= ~(1 << 7);  // Turn off LED (just an example, not necessary)
+    
+    //Linear actuator code
+        GPIOE->ODR |= (1 << 10);
+		delayMs(500);
+
+		GPIOE->ODR &= ~(1 << 10);
+		GPIOE->ODR |=  (1 << 12);
+		delayMs(500);
+
+		GPIOE->ODR &= ~(1 << 12);
+		GPIOE->ODR |=  (1 << 14);
+		delayMs(500);
+
+		GPIOE->ODR &= ~(1 << 14);
+		GPIOE->ODR |=  (1 << 15);
+		delayMs(500);
+
+		GPIOE->ODR &= ~(1 << 15);
+		delayMs(500);
+    //
 
     // Transition to the next state
     current_state = state_checkHeadPosition;
